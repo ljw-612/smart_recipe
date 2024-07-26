ef4c12a3829651f5b9f5ea71771b8d36bec2b659
Jingwei Li <lijingwei58466809@outlook.com>
2024-07-26 13:42:56 +0800

    streamlit app built

---
+++ script/app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import openai
+
+import pandas as pd
+import sqlite3
+
+import os
+from dotenv import load_dotenv
+
+from utils import *
+
+load_dotenv(override=True)
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+def generate_recipe(query):
+    dish = get_dish(query)
+    return dish
+
+def fetch_db(ingredients_db_path):
+    conn = sqlite3.connect(ingredients_db_path)
+    query = "SELECT * FROM ingredients"
+    df = pd.read_sql_query(query, conn)
+    return df
+
+def app():
+    ingredients_db_path = "../data/ingredients.db"
+    
+    
+    query = "请你根据我现有的食材以及食材数量，推荐【一道菜】。告诉我所需食材以及相对应的克数。"
+    
+    st.title("Recipe Recommendation System")
+    
+    st.write("#### Available Ingredients in the Database")
+    df = fetch_db(ingredients_db_path)
+    st.dataframe(df)
+    
+    if 'generated_recipe' not in st.session_state:
+        st.session_state['generated_recipe'] = ""
+
+    st.button("Generate a Recipe for Me", on_click=generate_recipe_callback, args=(query,))
+
+    if st.session_state['generated_recipe']:
+        st.write("## Generated Recipe")
+        st.text(st.session_state['generated_recipe'])
+
+        col1, col2 = st.columns(2)
+        with col1:
+            if st.button("Accept this Recipe"):
+                st.success("Recipe accepted!")
+                st.success("Ingredients removed from the database....")
+                remove_ingredients(st.session_state['generated_recipe'], ingredients_db_path)
+                st.session_state['generated_recipe'] = ""  # Reset recipe
+                st.dataframe(fetch_db(ingredients_db_path))
+        with col2:
+            if st.button("Regenerate"):
+                st.session_state['generated_recipe'] = generate_recipe(query)
+
+
+def generate_recipe_callback(query):
+    st.session_state['generated_recipe'] = generate_recipe(query)
+    
+
+if __name__ == "__main__":
+    app()
--- script/utils.py
+++ script/utils.py
@@ -8,6 +8,10 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.utils import DistanceStrategy
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.utilities.sql_database import SQLDatabase
+
 
 load_dotenv(override=True)
 openai_api_key = os.getenv("OPENAI_API_KEY")
@@ -23,7 +27,7 @@ def get_ingredients_from_db(db_path):
 def fetch_ingredients_content(db_path):
     ingredients_df = get_ingredients_from_db(db_path)
     ingredients_list = ingredients_df.to_dict('records')
-    context = "Here are the infredients available:\n"
+    context = "Here are the ingredients available:\n"
     for ingredient in ingredients_list:
         context += f"{ingredient['name']}: {ingredient['amount']}克\n"
     
@@ -51,9 +55,12 @@ def fetch_food_incompatibilities_content(db_path, aviailable_ingredients, k=5):
     return retrieved_docs
 
 def recommend_dish(query, ingredients_context, available_ingredients, incompatibitlies_context):
+    # client = OpenAI(
+    #     base_url="http://127.0.0.1:8080/v1",
+    #     api_key=openai_api_key
+    # )
     client = OpenAI(
-        base_url="http://127.0.0.1:8080/v1",
-        api_key=openai_api_key
+        api_key=openai_api_key,
     )
     retrieved_docs_text = [
         doc.page_content for doc in incompatibitlies_context
@@ -62,9 +69,9 @@ def recommend_dish(query, ingredients_context, available_ingredients, incompatib
     context += "".join(
         [f"Document {str(i)}:::\n" + doc for i, doc in enumerate(retrieved_docs_text)]
     )
-    print(ingredients_context)
-    print("----------------")
-    print(incompatibitlies_context[0].page_content)
+    # print(ingredients_context)
+    # print("----------------")
+    # print(incompatibitlies_context[0].page_content)
     RAG_PROMPT_TEMPLATE = [
         {
             "role": "system",
@@ -78,8 +85,15 @@ def recommend_dish(query, ingredients_context, available_ingredients, incompatib
             "role": "user",
             "content": """食物相克信息:
             {context}\n
+            
             我现有的食材:
             {ingredients_context}\n
+            ---
+            你需要严格通过以下格式返回结果：
+            ```
+            菜名：
+            食材和用量：
+            ```
             ---
             Now here is the question you need to answer.
             
@@ -91,25 +105,66 @@ def recommend_dish(query, ingredients_context, available_ingredients, incompatib
         },
     ]
     completion = client.chat.completions.create(
-        model="LLaMA_CPP",
+        # model="LLaMA_CPP",
+        model="gpt-3.5-turbo",
         messages=RAG_PROMPT_TEMPLATE,
         # temperature=0.1
         )
     return completion.choices[0].message.content
-        
+
+def remove_ingredients(dish, ingredients_db_path):
+    db = SQLDatabase.from_uri("sqlite:///" + ingredients_db_path)
+    llm = ChatOpenAI(
+        model="gpt-3.5-turbo",
+        api_key=openai_api_key,
+    )
     
+    agent_executor = create_sql_agent(
+        llm=llm,
+        db=db,
+        agent_type="openai-tools",
+        verbose=True,
+    )
+    
+    agent_executor.invoke(
+        {
+            "input": """根据菜谱，从数据库中移除被使用的材料以及相应的数量。
+                    菜谱：{dish}""".format(dish=dish),
+        }
+    )
+    print("Ingredients removed successfully.")
+
+def get_dish(query):
+    ingredients_db_path = "../data/ingredients.db"
+    food_incompatibilities_db_path = "../data/food_incomp"
+    
+    print("Fetching ingredients content...\n")
+    ingredients_context, available_ingredients = fetch_ingredients_content(ingredients_db_path)
+    
+    print("Fetching food incompatibilities content...\n")
+    incompatibitlies_context = fetch_food_incompatibilities_content(food_incompatibilities_db_path, available_ingredients)
+    
+    print("Recommend dish...\n")
+    dish = recommend_dish(query, ingredients_context, available_ingredients, incompatibitlies_context)
+    return dish
+
+
 def main(query):
     ingredients_db_path = "../data/ingredients.db"
     food_incompatibilities_db_path = "../data/food_incomp"
     
-    print("Fetching ingredients content...")
+    print("Fetching ingredients content...\n")
     ingredients_context, available_ingredients = fetch_ingredients_content(ingredients_db_path)
     
-    print("Fetching food incompatibilities content...")
+    print("Fetching food incompatibilities content...\n")
     incompatibitlies_context = fetch_food_incompatibilities_content(food_incompatibilities_db_path, available_ingredients)
     
-    response = recommend_dish(query,ingredients_context, available_ingredients, incompatibitlies_context)
-    print(response)
+    print("Recommend dish...\n")
+    dish = recommend_dish(query, ingredients_context, available_ingredients, incompatibitlies_context)
+    print(dish)
+
+    print("Removing ingredients...\n")
+    remove_ingredients(dish, ingredients_db_path)
 
 if __name__ == "__main__":
     query = "请你根据我现有的食材以及食材数量，推荐【一道菜】。告诉我所需食材以及相对应的克数。"
