c2175244e92b7834658ba9846321a4dda8a26828
Jingwei Li <lijingwei58466809@outlook.com>
2024-08-06 22:58:20 +0800

    Update test_get_dish.py

--- tests/test_get_dish.py
+++ tests/test_get_dish.py
@@ -7,10 +7,10 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from script.utils import get_dish
 
-def test_get_dish():
-    query = "请你根据我现有的食材以及食材数量，推荐【一道菜】。告诉我所需食材以及相对应的克数。"
-    dish = get_dish(query)
+# def test_get_dish():
+#     query = "请你根据我现有的食材以及食材数量，推荐【一道菜】。告诉我所需食材以及相对应的克数。"
+#     dish = get_dish(query)
     
-    assert type(dish) == str
-    assert "菜名" in dish
-    assert "食材和用量" in dish
+#     assert type(dish) == str
+#     assert "菜名" in dish
+#     assert "食材和用量" in dish
